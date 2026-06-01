c85b910214c723b771c8d8183f4e127c2ef35f08
Exaucé TOHOUN <developer3n@gmail.com>
2026-06-01 13:31:03 +0100

    Use Render backend API

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'https://Agnuod19-voicereader-backend.hf.space';
+export const API_BASE_URL = 'https://voicereader-backend.onrender.com';
 
 const API = axios.create({
   baseURL: API_BASE_URL,
